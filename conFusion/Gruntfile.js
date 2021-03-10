5277abaa8b861b9f0de1da9f3fed9bd3185ba37d
vitran96 <tranuytrieuvi@gmail.com>
2021-03-10 22:55:22 +0700

    add sass task

--- conFusion/Gruntfile.js
+++ conFusion/Gruntfile.js
@@ -1,9 +1,26 @@
 'use strict';
 
 module.exports = function(grunt) {
+    const sass = require('node-sass');
+
     require('time-grunt')(grunt);
+
     require('jit-grunt')(grunt);
+
     grunt.initConfig({
         // CONFIGURATION
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        }
     });
+
+    grunt.registerTask('css', ['sass']);
 };
