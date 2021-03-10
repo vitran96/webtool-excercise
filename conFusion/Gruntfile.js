17ab3db615c4b68742e3e839f0ba74ad433967ac
vitran96 <tranuytrieuvi@gmail.com>
2021-03-10 23:06:23 +0700

    add browsersync task

--- conFusion/Gruntfile.js
+++ conFusion/Gruntfile.js
@@ -11,16 +11,35 @@ module.exports = function(grunt) {
         // CONFIGURATION
         sass: {
             options: {
-                implementation: sass,
-                sourceMap: true
-            },
-            dist: {
+                implementation: sass
+                , sourceMap: true
+            }
+            , dist: {
                 files: {
                     'css/styles.css': 'css/styles.scss'
+                }
+            }
+        }, watch: {
+            files: 'css/*.scss'
+            , tasks: ['sass']
+        }, browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css'
+                        , '*.html'
+                        , 'js/*.js'
+                    ]
+                }, options: {
+                    watchTask: true
+                    , server: {
+                        baseDir: './'
+                    }
                 }
             }
         }
     });
 
     grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
 };
